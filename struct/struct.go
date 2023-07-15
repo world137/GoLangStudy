dee0ee59294117ac513d81a583b18e9296baea5c
world.cyp <59548715+world137@users.noreply.github.com>
2023-07-15 22:39:32 +0700

    Update struct.go

--- struct/struct.go
+++ struct/struct.go
@@ -33,3 +33,19 @@ func main() {
 	fmt.Println("Car1 : ", car1)
 	fmt.Println("Carlist : ", carlist)
 }
+type Vertex struct {
+	X, Y int
+}
+
+var (
+	v1 = Vertex{1, 2}  // has type Vertex
+	v2 = Vertex{X: 1}  // Y:0 is implicit x:0 => x =0
+	v3 = Vertex{}      // X:0 and Y:0
+	p  = &Vertex{1, 2} // has type *Vertex
+)
+
+func main() {
+	fmt.Println(v1, p, v2, v3)
+	fmt.Println(v2.X)
+	fmt.Println(v2.Y)
+}
